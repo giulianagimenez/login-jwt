ecd1f842679bc583a5fe67e88e013de046adc584
GiulianaGimenez <giulianaagimenez@gmail.com>
2024-11-26 21:24:13 -0300

    Diseño Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,20 +3,29 @@ import { AuthGuard } from './core/guards/auth.guard';
 import { authenticatedGuard } from './core/guards/authenticated.guard';
 
 export const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadComponent: () => import('./shared/components/layout/layout.component'),
-  //   children:[
-  //     {
-  //       path:'dashboard',
-  //       loadComponent: ()=> import('./business/dashboard/dashboard.component'),
-  //       canActivate: [AuthGuard]
-  //     }
-  //   ]
-  // },
+  {
+    path: '',
+    loadComponent: () => import('./shared/components/layout/layout.component'),
+    children:[
+      {
+        path:'dashboard',
+        loadComponent: ()=> import('./business/dashboard/dashboard.component'),
+        // canActivate: [AuthGuard]
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      }
+    ]
+  },
   {
     path: 'login',
     loadComponent: ()=>import('./private/auth/login/login.component'),
     canActivate: [authenticatedGuard]
+  },
+  {
+    path:'**',
+    redirectTo: 'dashboard'
   }
 ];
--- src/app/private/auth/login/login.component.ts
+++ src/app/private/auth/login/login.component.ts
@@ -3,16 +3,18 @@ import { AuthService } from '../../../core/services/auth.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export default class LoginComponent {
   user:string = '';
   password:string = '';
+  public passwordTextType = false;
 
   constructor(private authService: AuthService, private router: Router){}
 
@@ -25,5 +27,9 @@ export default class LoginComponent {
 
   logout():void {
     this.authService.logout();
+  }
+
+  public togglePasswordTextType() {
+    this.passwordTextType = !this.passwordTextType;
   }
 }
